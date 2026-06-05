b91ce2777d5e32bf014c5d3da4ac92dc30f1d3a6
Raheen Zia <raheenzu02@gmail.com>
2026-06-06 02:47:26 +0500

    commit

---
+++ config.py
@@ -0,0 +1,31 @@
+EXPERIMENT_CONFIGS = {
+    'Baseline': {
+        'use_pos': False,
+        'use_synonyms': False,
+        'hypernym_depth': 0
+    },
+    'PoS_Only': {
+        'use_pos': True,
+        'use_synonyms': False,
+        'hypernym_depth': 0
+    },
+    'Syns': {
+        'use_pos': True,
+        'use_synonyms': True,
+        'hypernym_depth': 0
+    },
+    'Hyper_5': {
+        'use_pos': True,
+        'use_synonyms': True,
+        'hypernym_depth': 5
+    },
+    'Hyper_All': {
+        'use_pos': True,
+        'use_synonyms': True,
+        'hypernym_depth': -1  # -1 means all hypernyms
+    }
+}
+
+CLUSTER_SIZES = [16, 32, 64]
+NUM_RUNS = 3  
+RANDOM_SEED = 42
